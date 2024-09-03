2e61c1fedf7e2f85a8f0ab545697de1eacd860cb
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2024-09-03 09:54:35 +0100

    Add files via upload

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2024 at 06:57 AM
+-- Generation Time: Sep 03, 2024 at 10:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -38,16 +38,8 @@ CREATE TABLE `users` (
   `address` varchar(255) DEFAULT NULL,
   `language_preference` varchar(50) DEFAULT 'English',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `role_id` int(11) DEFAULT NULL
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `password`, `DOB`, `gender`, `tel`, `email`, `address`, `language_preference`, `created_at`, `updated_at`, `role_id`) VALUES
-(4, 'admin', 'admin', NULL, 'prefer_not_to_say', '0000000000', 'admin@example.com', NULL, 'English', '2024-08-19 09:11:12', '2024-08-19 09:11:12', NULL);
 
 --
 -- Indexes for dumped tables
@@ -58,8 +50,7 @@ INSERT INTO `users` (`id`, `name`, `password`, `DOB`, `gender`, `tel`, `email`, 
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `fk_users_role` (`role_id`);
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -69,17 +60,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `fk_users_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
